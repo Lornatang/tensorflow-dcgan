c0e5262b00da9d644a533dccba649734a2299c1a
Lornatang <shiyipaisizuo@gmail.com>
2018-11-10 15:46:09 +0800

    Update sp.py

--- sp.py
+++ sp.py
@@ -1,20 +1,23 @@
 from PIL import Image
-import sys
 
 
 def fill_image(image):
     width, height = image.size
     print(width, height)
-    
+
     new_image_length = width if width > height else height
-    
+
     print(new_image_length)
 
     # new_image = Image.new(image.mode, (new_image_length, new_image_length), color='white')
-    new_image = Image.new(image.mode, (new_image_length, new_image_length), color='white')
-    
+    new_image = Image.new(
+        image.mode,
+        (new_image_length,
+         new_image_length),
+        color='white')
+
     if width > height:
-        
+
         new_image.paste(image, (0, int((new_image_length - height) / 2)))
     else:
         new_image.paste(image, (int((new_image_length - width) / 2), 0))
@@ -29,7 +32,11 @@ def cut_image(image):
     for j in range(0, 8):
         for i in range(0, 8):
             count += 1
-            box = (i * item_width, j * item_width, (i + 1) * item_width, (j + 1) * item_width)
+            box = (
+                i * item_width,
+                j * item_width,
+                (i + 1) * item_width,
+                (j + 1) * item_width)
             box_list.append(box)
     print(count)
     image_list = [image.crop(box) for box in box_list]
